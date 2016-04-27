f78247587b643c8d0629db751e1a0a358bf91e60
Jesslly Wong <jesslly.wong@netsuite.com>
2016-04-27 16:53:11 -0700

    first commit

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Wong, Jesslly on 2016-04-27.
+//  Copyright © 2016 Wong, Jesslly. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+}
---
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,63 @@
+//
+//  FlickrPhotosViewControllerCollectionViewController.swift
+//  FlickrSearch
+//
+//  Created by Wong, Jesslly on 2016-04-22.
+//  Copyright © 2016 Wong, Jesslly. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UICollectionViewController {
+
+    private let reuseIdentifier = "FlickrCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    private var searches = [FlickrSearchResults]()
+    private let flickr = Flickr()
+    
+    func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+   
+}
+
+
+extension FlickrPhotosViewController : UITextFieldDelegate {
+    
+   func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle : .Gray)
+        
+        textField.addSubview(activityIndicator)
+        
+        activityIndicator.frame = textField.bounds
+        
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            //2
+            activityIndicator.removeFromSuperview()
+            
+            if error != nil {
+                print("Error searching : \(error)")
+            }
+            
+            if results != nil {
+                //3
+                print("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+                self.searches.insert(results!, atIndex: 0)
+                
+                //4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+        
+    }
+}
